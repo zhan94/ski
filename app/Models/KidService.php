ca516778613094b81e45cafc3c70dd552b2e24e0
zhan94 <jan94web@gmail.com>
2022-11-06 18:03:03 +0100

    add models

--- app/Models/KidService.php
+++ app/Models/KidService.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\hasOne;
 
 class KidService extends Model
 {
     use HasFactory;
+
+    public function kidName(): hasOne
+    {
+        return $this->hasOne(Kid::class, 'id', 'kid_id')->select(['id', 'firstname', 'lastname', 'birth_date']);
+    }
+
+    public function kid(): hasOne
+    {
+        return $this->hasOne(Kid::class, 'id', 'kid_id');
+    }
+
+    public function location(): hasOne
+    {
+        return $this->hasOne(Location::class, 'id', 'location_id');
+    }
 }
--- app/Models/ServicesData.php
+++ app/Models/ServicesData.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ServicesData extends Model
 {
@@ -12,6 +13,13 @@ class ServicesData extends Model
 
     public function kids(): HasMany
     {
-        return $this->hasMany(KidService::class);
+        $select = ['id', 'services_data_id', 'kid_id', 'location_id', 'dates'];
+        return $this->hasMany(KidService::class)->select($select);
     }
+
+    public function service(): hasOne
+    {
+        return $this->hasOne(Service::class, 'id', 'service_id')->select(['id', 'name as service_name']);
+    }
+
 }
